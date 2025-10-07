9f3392c8087e012fb5204d7798c9bd57f8691b8b
ProgKenhy <sashok20053@gmail.com>
2025-10-07 17:39:06 +0200

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parent))
+
+import uvicorn
+from fastapi import FastAPI
+from core.routers import api_router
+
+app = FastAPI()
+
+app.include_router(api_router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
