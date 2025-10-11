34c057f3d06fb2a4d1c95f99108121e29bf6eb34
ProgKenhy <sashok20053@gmail.com>
2025-10-11 15:22:54 +0200

    fix useless taking user

--- src/auth/deps.py
+++ src/auth/deps.py
@@ -3,26 +3,30 @@ from typing import Annotated, Optional
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 
-from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.deps import get_db_session
 from users.models import User
 from .utils import decode_token
+from users.crud import get_user_by_id
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/token")
 
 
+async def get_user_id_from_token(token: str = Depends(oauth2_scheme)) -> Optional[int]:
+    """Получение user_id из Token"""
+    token_data = decode_token(token)
+    return int(token_data.sub)
+
+
 async def get_user_by_token(
         token: Annotated[str, Depends(oauth2_scheme)],
-        db_session: Annotated[AsyncSession, Depends(get_db_session)],
+        db: Annotated[AsyncSession, Depends(get_db_session)],
 ) -> Optional[User]:
     """Получение текущего пользователя по Token"""
     token_data = decode_token(token)
 
-    stmt = select(User).where(User.id == int(token_data.sub))
-    user = (await db_session.execute(stmt)).scalar_one_or_none()
-
+    user = await get_user_by_id(user_id=int(token_data.sub), db=db)
     if user is None:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -30,4 +34,3 @@ async def get_user_by_token(
             headers={"WWW-Authenticate": "Bearer"},
         )
     return user
-
--- src/tickets/routers.py
+++ src/tickets/routers.py
@@ -2,43 +2,49 @@ from fastapi import APIRouter, Depends, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Annotated, Optional
 
-from auth.deps import get_user_by_token
+from auth.deps import get_user_id_from_token
 from core.deps import get_db_session
 from users.models import User
 from .models import TicketStatus
 from .schemas import TicketUpdate, TicketCreate, TicketResponse, TicketDeleteResponse
 from .crud import get_tickets_crud
 from .service import update_ticket_service, create_ticket_service, delete_ticket_service
+from core.redis import make_cache_key, get_cached_or_set
 
 ticket_router = APIRouter()
 
 
 @ticket_router.post("/", response_model=TicketResponse)
 async def create_ticket_endpoint(ticket: TicketCreate,
-                                 user: Annotated[User, Depends(get_user_by_token)],
+                                 user_id: Annotated[int, Depends(get_user_id_from_token)],
                                  db: Annotated[AsyncSession, Depends(get_db_session)]) -> TicketResponse:
-    ticket = await create_ticket_service(ticket_data=ticket, user_id=user.id, db=db)
+    ticket = await create_ticket_service(ticket_data=ticket, user_id=user_id, db=db)
     return TicketResponse.model_validate(ticket)
 
 
 @ticket_router.get("/", response_model=list[TicketResponse])
-async def get_tickets_endpoint(user: Annotated[User, Depends(get_user_by_token)],
+async def get_tickets_endpoint(user_id: Annotated[int, Depends(get_user_id_from_token)],
                                db: Annotated[AsyncSession, Depends(get_db_session)],
                                statuses: Optional[list[TicketStatus]] = Query(None)
                                ) -> list[TicketResponse]:
-    tickets = await get_tickets_crud(user_id=user.id, statuses=statuses, db=db)
+    key = make_cache_key("cache:ticket:list", "/ticket", user_id,
+                         statuses)  # в дальнейшем можно спрятать в get_cached_or_set
+    async def loader():
+        return await get_tickets_crud(user_id=user_id, statuses=statuses, db=db)
+
+    tickets = await get_cached_or_set(key, loader)
     return [TicketResponse.model_validate(ticket) for ticket in tickets]
 
 
 @ticket_router.put("/", response_model=TicketResponse)
 async def update_ticket_endpoint(ticket_update: TicketUpdate,
-                                 user: Annotated[User, Depends(get_user_by_token)],
+                                 user_id: Annotated[int, Depends(get_user_id_from_token)],
                                  db: Annotated[AsyncSession, Depends(get_db_session)]) -> TicketResponse:
-    ticket = await update_ticket_service(ticket_update=ticket_update, user_id=user.id, db=db)
+    ticket = await update_ticket_service(ticket_update=ticket_update, user_id=user_id, db=db)
     return TicketResponse.model_validate(ticket)
 
 
 @ticket_router.delete("/", response_model=TicketDeleteResponse)
-async def delete_ticket_endpoint(ticket_id: int, user: Annotated[User, Depends(get_user_by_token)],
+async def delete_ticket_endpoint(ticket_id: int, user_id: Annotated[int, Depends(get_user_id_from_token)],
                                  db: Annotated[AsyncSession, Depends(get_db_session)]) -> TicketDeleteResponse:
-    return await delete_ticket_service(ticket_id=ticket_id, user_id=user.id, db=db)
+    return await delete_ticket_service(ticket_id=ticket_id, user_id=user_id, db=db)
