f679cde7c1c1e7189565cc9de1e20f00338640d6
ProgKenhy <sashok20053@gmail.com>
2025-10-11 15:21:39 +0200

    add sessions

--- src/auth/routers.py
+++ src/auth/routers.py
@@ -1,7 +1,9 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request, Response
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from .auth_session import update_session_when_login, delete_session
 from .schemas import Token
 from core.deps import get_db_session
 from .utils import create_access_token
@@ -13,10 +15,18 @@ auth_router = APIRouter()
 
 @auth_router.post("/token")
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-                                 db: Annotated[AsyncSession, Depends(get_db_session)]) -> Token:
-    """Ручка для входа пользователя по username/email и паролю с созданием access_token"""
+                                 db: Annotated[AsyncSession, Depends(get_db_session)], request: Request) -> Token:
+    """Ручка для входа пользователя по username/email и паролю с созданием access_token и обновлением информации о сессии"""
     user = await authenticate_user(form_data.username, db, form_data.password)
+    await update_session_when_login(request, user.id) # noqa
     token_str = create_access_token(
         subject=str(user.id),
     )
     return Token(access_token=token_str, token_type="bearer")
+
+
+@auth_router.post("/logout")
+async def logout(response: Response, request: Request):
+    """Выход пользователя - очистка сессии"""
+    await delete_session(response=response, request=request)
+    return {"message": "Logged out successfully"}
--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel
 from datetime import datetime
 
@@ -13,3 +15,25 @@ class TokenData(BaseModel):
     sub: str
     exp: datetime
     type: str
+
+
+class SessionData(BaseModel):
+    """Модель данных сессии"""
+    created_at: str
+    is_authenticated: bool = False
+
+    user_id: Optional[int] = None
+    email: Optional[str] = None
+    username: Optional[str] = None
+
+
+class SessionCreate(SessionData):
+    """Модель для создания новой сессии"""
+    pass
+
+class SessionUpdate(BaseModel):
+    """Модель для обновления сессии"""
+    user_id: Optional[int] = None
+    email: Optional[str] = None
+    username: Optional[str] = None
+    is_authenticated: Optional[bool] = None
